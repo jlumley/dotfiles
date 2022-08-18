5564ddb0a2a07ff7533835df0caff24563366fe9
Jeremy Lumley <Jeremy.lumley96@gmail.com>
2022-08-18 12:17:59 -0400

    adding more vim remaps

--- nvim/nvim/lua/jlumley/remap.lua
+++ nvim/nvim/lua/jlumley/remap.lua
@@ -1,7 +1,20 @@
+local expr_opts = { noremap = true, silent = true, expr = true }
+
+
 local function map(mode, lhs, rhs, opts)
 local options = {noremap = true}
     if opts then options = vim.tbl_extend('force', options, opts) end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- rempas for fzf
 map("n", "<C-p>", "<cmd>FZF<CR>")
+
+-- remaps for coc
+map(
+    "i",
+    "<CR>",
+    [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
+    expr_opts
+)
+
