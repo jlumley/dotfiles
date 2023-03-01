1ee6c6e148625c4e7837318c7393efa86c967042
Jeremy Lumley <jlumley@tuwcows.com>
2023-03-01 16:32:56 -0500

    adding tmux and updating vim config

--- nvim/nvim/lua/jlumley/packer.lua
+++ nvim/nvim/lua/jlumley/packer.lua
@@ -2,40 +2,84 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  -- Tokyo night colour scheme
-  use 'folke/tokyonight.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- Tokyo night colour scheme
+    use 'folke/tokyonight.nvim'
 
-  use {
-    'vimwiki/vimwiki',
-    config = function()
-    local l = {}
-    l.syntax = 'markdown'
-    l.ext = '.md'
-    l.auto_diary_index = 1
-    vim.g.vimwiki_list = {l}
-    end
-  }
-
-  use 'airblade/vim-gitgutter'
-  use 'sbdchd/neoformat'
-  use 'stefandtw/quickfix-reflector.vim'
-  use 'tomtom/tcomment_vim'
-  use 'tpope/vim-fugitive'
-  use 'vim-airline/vim-airline'
-  use { 'junegunn/fzf', run = function() vim.fn['fzf#install']() end}
-  use {'neoclide/coc.nvim', branch = 'release'}
-
-  use {
-  "folke/todo-comments.nvim",
-  requires = "nvim-lua/plenary.nvim",
-  config = function()
-    require("todo-comments").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+    use {
+        'vimwiki/vimwiki',
+        config = function()
+            local l = {}
+            l.syntax = 'markdown'
+            l.ext = '.md'
+            l.auto_diary_index = 1
+            vim.g.vimwiki_list = {l}
+        end
     }
-  end
-  }
+
+    use 'airblade/vim-gitgutter'
+
+    use 'stefandtw/quickfix-reflector.vim'
+
+    use 'tomtom/tcomment_vim'
+
+    use 'tpope/vim-fugitive'
+
+    use 'vim-airline/vim-airline'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    -- diabling coc to trest nvim built-in lsp
+    -- use {'neoclide/coc.nvim', branch = 'release'}
+
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+            require("todo-comments").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+
+    --install language server client
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
+
+
 end)
--- nvim/nvim/lua/jlumley/remap.lua
+++ nvim/nvim/lua/jlumley/remap.lua
@@ -6,9 +6,17 @@ local options = {noremap = true}
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- rempas for fzf
-map("n", "<C-p>", "<cmd>FZF<CR>")
+-- remaps for fzf
+--map("n", "<C-p>", "<cmd>FZF<CR>")
 
+-- remaps for telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 map("n", "<leader>d", ":pu=strftime('%B %d, %Y')<CR>")
 
 -- remaps for coc
