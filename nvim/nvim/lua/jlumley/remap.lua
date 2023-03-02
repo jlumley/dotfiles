634bef122254c80413776e25dfee0c5f2728cb47
Jeremy Lumley <jlumley@tuwcows.com>
2023-03-02 15:07:36 -0500

    clean up

--- nvim/nvim/lua/jlumley/remap.lua
+++ nvim/nvim/lua/jlumley/remap.lua
@@ -1,14 +1,5 @@
 local expr_opts = { noremap = true, silent = true, expr = true }
 
-local function map(mode, lhs, rhs, opts)
-local options = {noremap = true}
-    if opts then options = vim.tbl_extend('force', options, opts) end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
--- remaps for fzf
---map("n", "<C-p>", "<cmd>FZF<CR>")
-
 -- remaps for telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
@@ -17,7 +8,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-map("n", "<leader>d", ":pu=strftime('%B %d, %Y')<CR>")
 
 -- move highlight with J K
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -27,18 +17,11 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- yank to system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- delete to void register
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- format file with lsp
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
--- remaps for moving through quickfix
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
